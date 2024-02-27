e407649fea534ea66a1fc7ee3eda3a31157c2171
gecko10000 <levtey@gmail.com>
2024-02-26 19:30:54 -0800

    Centralize Component#withDefaults

---
+++ src/gecko10000/geckolib/extensions/ExComponent.kt
@@ -0,0 +1,8 @@
+package gecko10000.geckolib.extensions
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
+
+fun Component.withDefaults(color: TextColor = NamedTextColor.WHITE) = this.decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE).colorIfAbsent(color)
