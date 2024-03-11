42ae53346edb9a560fd5e228d0eac72bc7bee3ca
gecko10000 <levtey@gmail.com>
2024-03-11 00:39:49 -0700

    Send reloading message with delay

--- src/gecko10000/geckolib/GeckoLib.kt
+++ src/gecko10000/geckolib/GeckoLib.kt
@@ -4,6 +4,9 @@ import gecko10000.geckolib.extensions.MM
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import redempt.redlib.misc.Task
+import java.util.Timer
+import java.util.TimerTask
 
 class GeckoLib : JavaPlugin() {
     override fun onDisable() {
@@ -11,7 +14,13 @@ class GeckoLib : JavaPlugin() {
         .filter { it.openInventory.topInventory.holder is GUI }
         .forEach { player ->
             player.closeInventory()
-            player.sendMessage(MM.deserialize("<red>Plugin reloading."))
+            // TODO: maybe just remove this?
+            Timer().schedule(object : TimerTask() {
+                override fun run() {
+                    player.sendMessage(MM.deserialize("<red>Plugin reloading."))
+                }
+
+            }, 1000L)
         }
     }
 }
