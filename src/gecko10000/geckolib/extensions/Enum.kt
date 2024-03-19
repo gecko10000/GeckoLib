127536f79fe5686938d962aaa1237caa8ebe0434
gecko10000 <levtey@gmail.com>
2024-03-18 22:37:27 -0700

    Add isMax for enums

--- src/gecko10000/geckolib/extensions/Enum.kt
+++ src/gecko10000/geckolib/extensions/Enum.kt
@@ -1,6 +1,11 @@
 package gecko10000.geckolib.extensions
 inline fun <reified T : Enum<T>> T.next(): T {
     val values = enumValues<T>()
-    val next = (ordinal + 1) % values.size
+    val next = (this.ordinal + 1) % values.size
     return values[next]
+}
+
+inline fun <reified T : Enum<T>> T.isMax(): Boolean {
+    val count = enumValues<T>().size
+    return this.ordinal == count - 1
 }
