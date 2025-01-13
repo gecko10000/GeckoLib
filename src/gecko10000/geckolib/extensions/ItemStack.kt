f149d791240dbc59f8f5aa4bc770f7a5df0bcc2b
gecko10000 <levtey@gmail.com>
2025-01-13 15:36:00 -0600

    Add extension to extract item name

--- src/gecko10000/geckolib/extensions/ItemStack.kt
+++ src/gecko10000/geckolib/extensions/ItemStack.kt
@@ -1,7 +1,17 @@
 package gecko10000.geckolib.extensions
 
+import net.kyori.adventure.text.Component
 import org.bukkit.inventory.ItemStack
 
 fun ItemStack?.isEmpty(): Boolean {
     return this == null || this.isEmpty
+}
+
+fun ItemStack.name(): Component {
+    val meta = this.itemMeta
+    val backupName = if (meta.hasItemName())
+        meta.itemName()
+    else
+        Component.translatable(this.translationKey())
+    return meta.customName() ?: backupName
 }
