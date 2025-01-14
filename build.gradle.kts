eaac4946812555ba48bfac761e6b4858a32c7648
gecko10000 <levtey@gmail.com>
2025-01-13 21:25:28 -0600

    Make stdlib a library

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,11 @@ repositories {
 }
 
 dependencies {
-    api(kotlin("stdlib", version = "2.0.21"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    compileOnly(kotlin("stdlib", version = "2.0.21")) // From libraries in plugin.yml
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("net.kyori:adventure-platform-bukkit:4.3.2")
     compileOnly("com.github.Redempt:RedLib:6.6.1")
+
     api("io.insert-koin:koin-core:3.5.3") {
         exclude("org.jetbrains.kotlin")
     }
