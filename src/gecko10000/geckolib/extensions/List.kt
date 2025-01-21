3bdf53f7d2b637e76ef8580342d7464f56912f55
gecko10000 <levtey@gmail.com>
2025-01-21 02:31:13 -0600

    Add List#extend method

--- src/gecko10000/geckolib/extensions/List.kt
+++ src/gecko10000/geckolib/extensions/List.kt
@@ -9,3 +9,9 @@ fun <T> List<T>.updated(index: Int, value: T): List<T> {
         this.addAll(this@updated.subList(index + 1, this@updated.size))
     }
 }
+
+fun <T> List<T>.extend(element: T, amount: Int? = null, targetAmount: Int? = null): List<T> {
+    val toAdd = amount ?: (targetAmount!! - this.size)
+    if (toAdd <= 0) return this
+    return this.plus(List(toAdd) { element })
+}
